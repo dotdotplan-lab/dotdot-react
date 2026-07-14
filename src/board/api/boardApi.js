55ffca94c2047d631af33f14628cfd6e113d88d2
hhmkorea <dotdot_plan@daum.net>
2026-07-14 17:41:44 +0900

    fit: 명명규칙 수정.

--- src/board/api/boardApi.js
+++ src/board/api/boardApi.js
@@ -1,7 +1,7 @@
 import { board } from '../../shared/api/http.js';
 
-export const getBoardList = () => board.get('/boardlist').then((res) => res.data.data);
+export const getBoardList = () => board.get('/list').then((res) => res.data.data);
 export const getBoardDetail = (id) => board.get(`/${id}`).then((res) => res.data.data);
-export const createBoard = (payload) => board.post('/saveBoard', payload).then((res) => res.data.data);
+export const createBoard = (payload) => board.post('/create', payload).then((res) => res.data.data);
 export const updateBoard = ({ id, payload }) => board.put(`/${id}`, payload).then((res) => res.data.data);
 export const deleteBoard = (id) => board.delete(`/${id}`).then((res) => res.data.data);
