7c893c24decf024427a6705ee7f59fe5f703a09d
hhmkorea <dotdot_plan@daum.net>
2026-07-16 17:42:49 +0900

    fit: API 호출 명명규칙 saveXXX로 변경

--- src/canvas/api/canvasApi.js
+++ src/canvas/api/canvasApi.js
@@ -2,13 +2,13 @@ import { canvas } from '../../shared/api/http.js';
 
 export const getCanvasList = (params) => canvas.get('/canvaslist', { params }).then((res) => res.data.data); // 캔버스 목록
 export const getCanvasDetail = (id) => canvas.get(`/${id}`).then((res) => res.data.data); // 캔버스 상세, 노트 정보 가져옴.
-export const createCanvas = (payload) => canvas.post('/createCanvas', payload).then((res) => res.data.data);
+export const createCanvas = (payload) => canvas.post('/saveCanvas', payload).then((res) => res.data.data);
 export const updateCanvas = ({ id, payload }) => canvas.put(`/${id}`, payload).then((res) => res.data.data);
 export const deleteCanvas = (id) => canvas.delete(`/${id}`).then((res) => res.data.data);
 
 export const getCanvasNoteList = (params) => canvas.get('/note/list', { params }).then((res) => res.data.data);
 export const getCanvasNoteDetail = (id) => canvas.get(`/note/${id}`).then((res) => res.data.data);
-export const createCanvasNote = (payload) => canvas.post('/note/createNote', payload).then((res) => res.data.data);
+export const createCanvasNote = (payload) => canvas.post('/note/saveCanvasNote', payload).then((res) => res.data.data);
 export const updateCanvasNote = ({ id, payload }) => canvas.put(`/note/${id}`, payload).then((res) => res.data.data);
 export const deleteCanvasNote = (id) => canvas.delete(`/note/${id}`).then((res) => res.data.data);
 
