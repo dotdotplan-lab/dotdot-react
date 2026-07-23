d76e5eddb4b2ae2551245e1a98a1f3b8db1c3456
hhmkorea <dotdot_plan@daum.net>
2026-07-23 18:25:50 +0900

    .

--- src/contents/shop/Shop.jsx
+++ src/contents/shop/Shop.jsx
@@ -7,7 +7,7 @@ function Shop() {
   return (
       <div className="container mx-auto px-4 py-8">
           <div className="flex items-center justify-between mb-4">
-              <h1 className="text-2xl font-bold text-gray-800">상품 관리</h1>
+              <h1 className="text-2xl font-bold text-gray-800">상품 관리(... 준비중 ...)</h1>
           </div>
           <hr className="border-gray-300 mb-6" />
           <ProductMngt />
