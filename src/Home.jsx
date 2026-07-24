0d3dc61568252747284d1401a644ffe4ff62478c
hhmkorea <dotdot_plan@daum.net>
2026-07-24 16:41:50 +0900

    .

--- src/Home.jsx
+++ src/Home.jsx
@@ -25,7 +25,7 @@ function Home() {
       langs: "SQL",
       devenv: ["MariaDB"],
       batch: "Docker Container (AWS EC2)",
-      url: "",
+      url: "https://youtu.be/kq732RJaT4s",
     },
   ];
 
